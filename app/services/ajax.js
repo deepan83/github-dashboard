0e4db7ee132688a1482a06d710782719c67f1ccb
Deepan <deepan.cts@gmail.com>
2016-10-23 11:51:07 +0100

    update property

--- app/services/ajax.js
+++ app/services/ajax.js
@@ -3,10 +3,11 @@ import AjaxService from 'ember-ajax/services/ajax';
 
 export default AjaxService.extend({
   session: Ember.inject.service(),
-  headers: Ember.computed('session.data.accessToken', {
+  headers: Ember.computed('session.data.authenticated.accessToken', {
     get() {
       let headers = {};
-      const accessToken = this.get('session.data.accessToken');
+      const accessToken = this.get('session.data.authenticated.accessToken');
+
       if (accessToken) {
         headers['Authorization'] = `token ${accessToken}`;
       }
