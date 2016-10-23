1df4e3ca5461e1757678286762107568571fa683
Deepan <deepan.cts@gmail.com>
2016-10-23 12:50:08 +0100

    add fascia route

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  ajax: Ember.inject.service(),
 
   actions: {
     authenticate() {
       this.get('session').authenticate('authenticator:torii', 'github-oauth2')
       .then(() => {
-        this.transitionToRoute('index');
+        this.transitionToRoute('fascia');
       })
       .catch((reason) => {
         this.set('errorMessage', reason.error || reason);
--- app/routes/fascia.js
+++ app/routes/fascia.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  ajax: Ember.inject.service(),
+  model() {
+    const ajax = this.get('ajax');
+    return ajax
+      .request('https://api.github.com/user')
+      .then((user) => {
+        return ajax.request(`https://api.github.com/orgs/ConnectedHomes/members/${user.login}`)
+          .then(() => {
+            return user;
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
