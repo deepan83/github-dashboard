8996eca75f2192ed8119c135ca3c344b8d054869
Deepan <deepan.cts@gmail.com>
2016-10-23 10:33:58 +0100

    custom open method

--- app/torii-providers/github-oauth2.js
+++ app/torii-providers/github-oauth2.js
@@ -8,18 +8,18 @@ export default GithubOauth2Provider.extend({
   },
 
   open() {
-    return this._super();
-    
-    // .then((toriiData) => {
-    //   const authCode = toriiData.authorizationCode;
-    //   const serverUrl =  `https://github.com/login/oauth/access_token?client_id=32b96c8ef042cebd8842&client_secret=d4de1e4a71e1e4db920a484cb18fa05cbea2edc3&code=${authCode}`;
-    //
-    //   return this.get('ajax').post(serverUrl)
-    //   .then((data) => {
-    //     toriiData.accessToken = data.token;
-    //     return toriiData;
-    //   });
-    // });
+    return this._super()
+
+    .then((toriiData) => {
+      const authCode = toriiData.authorizationCode;
+      const serverUrl =  `https://github-cockpit.herokuapp.com/token?code=${authCode}`;
+
+      return this.get('ajax').post(serverUrl)
+      .then((data) => {
+        toriiData.accessToken = data.token;
+        return toriiData;
+      });
+    });
   }
 
 });
