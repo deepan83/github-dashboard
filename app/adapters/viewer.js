a29fc98015f0b77107ef0085ca62a7bbd9535bb4
Deepan <deepan.cts@gmail.com>
2016-10-24 22:23:07 +0100

    change bearer

--- app/adapters/viewer.js
+++ app/adapters/viewer.js
@@ -4,7 +4,7 @@ export default GraphQLAdapter.extend({
   session: Ember.inject.service(),
   endpoint: `${EmberENV.apiBaseUrl}/graphql`,
   headers: Ember.computed('session.data.authenticated.accessToken', function() {
-    const Authorization = `token ${this.get('session.data.authenticated.accessToken')}`;
+    const Authorization = `bearer ${this.get('session.data.authenticated.accessToken')}`;
 
     return {
       Authorization
