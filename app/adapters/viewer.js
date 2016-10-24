9c689d6e5eb0a396f7ca68927c8524a63b38d104
Deepan <deepan.cts@gmail.com>
2016-10-24 22:39:10 +0100

    change http method

--- app/adapters/viewer.js
+++ app/adapters/viewer.js
@@ -2,6 +2,7 @@ import GraphQLAdapter from 'ember-graphql-adapter';
 
 export default GraphQLAdapter.extend({
   session: Ember.inject.service(),
+  httpMethod: 'POST',
   endpoint: `${EmberENV.apiBaseUrl}/graphql`,
   headers: Ember.computed('session.data.authenticated.accessToken', function() {
     const Authorization = `bearer ${this.get('session.data.authenticated.accessToken')}`;
