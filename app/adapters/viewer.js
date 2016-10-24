0ac36495474d7bed5a71bb4b4cb6139f2a8ad765
Deepan <deepan.cts@gmail.com>
2016-10-24 22:11:40 +0100

    rename to viewer

---
+++ app/adapters/viewer.js
@@ -0,0 +1,13 @@
+import GraphQLAdapter from 'ember-graphql-adapter';
+
+export default GraphQLAdapter.extend({
+  session: Ember.inject.service(),
+  endpoint: `${EmberENV.apiBaseUrl}/graphql`,
+  headers: Ember.computed('session.data.authenticated.accessToken', function() {
+    const Authorization = `token ${this.get('session.data.authenticated.accessToken')}`;
+
+    return {
+      Authorization
+    };
+  })
+});
