341e195a922620a1a76c90666dd72362e7dd210b
Deepan <deepan.cts@gmail.com>
2016-10-28 15:53:29 +0100

    socket.io

--- app/routes/pull-requests.js
+++ app/routes/pull-requests.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
+  socketIOService: Ember.inject.service('socket-io'),
   model() {
     const repositories = ['oam', 'bg-gaq-and-checkout', 'bg-home-services', 'home-move', 'retention', 'hi'];
     const ajax = this.get('ajax');
@@ -12,11 +13,18 @@ export default Ember.Route.extend({
         .request(`https://api.github.com/repos/ConnectedHomes/${repository}/pulls`);
     });
 
+    const socket = this.get('socketIOService').socketFor('https://github-cockpit.herokuapp.com/');
+    socket.on('message', this.onMessage, this);
+
     return new Ember.RSVP.map(pullRequestPromises, (pullRequests) => {
       return {
         name: pullRequests.get('firstObject.head.repo.name'),
         pullRequests,
       };
     });
+  },
+
+  onMessage() {
+    console.log('hello!!!!!');
   }
 });
