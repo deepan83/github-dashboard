b5d708ff3d490494f69975e4ca76237becdfbc57
Jack Breakell <Jack@Admins-MacBook-Pro-2.local>
2016-12-06 13:53:57 +0000

    Add release page (dynamic)

--- app/routes/releases.js
+++ app/routes/releases.js
@@ -4,15 +4,17 @@ export default Ember.Route.extend( {
   ajax: Ember.inject.service(),
   model() {
     const repositories = ['oam', 'bg-gaq-and-checkout', 'bg-home-services', 'home-move', 'retention', 'hi'];
-    const ajax = this.get('ajax');
-    return ajax
-      .request('https://api.github.com/repos/ConnectedHomes/oam/releases/latest')
-      .then((response) => {
-        return [response];
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    let releases = repositories.map((repository) => {
+      const ajax = this.get('ajax');
+      return ajax
+        .request(`https://api.github.com/repos/ConnectedHomes/${repository}/releases/latest`)
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
+    return Ember.RSVP.all(releases);
 
     // return this.store.queryRecord('viewer', {});
   }
